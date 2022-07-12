b91fab008ffa8ce9ff2434e25c8d85411606649b
elsadany23 <mohammed.elsadany@sonono.ch>
2022-07-12 08:10:03 +0200

    hotel management api

---
+++ main.go
@@ -0,0 +1,28 @@
+// basic server
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/krishpranav/golang-management/middleware"
+	"github.com/krishpranav/golang-management/routers"
+)
+
+// server is running[now we can able to create users]
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routers.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routers.FoodRoutes(router)
+	routers.MenuRouters(router)
+
+	router.Run(":" + port)
+}
